bce561d0f891a68af9fa3e1da9749436cfb0c191
neo-liu <29376432+neo-liu@users.noreply.github.com>
2019-12-15 15:37:01 +0800

    Update main.py

--- Mechine-Learning/RNN with 1D input-data/main.py
+++ Mechine-Learning/RNN with 1D input-data/main.py
@@ -1,11 +1,11 @@
 import torch
+from torch import nn
 import numpy as np
 import matplotlib.pyplot as plt
-from torch import nn
 
-TIME_STEP = 20
+TIME_STEP = 50
 INPUT_SIZE = 1
-LearningRate = 0.0002
+LR = 0.0002
 
 
 class RNN(nn.Module):
@@ -20,29 +20,28 @@ class RNN(nn.Module):
         self.fc = nn.Linear(64, 1)
 
     def forward(self, x, h_state):
-        # x (batch, time_step, input_size)
-        # h_state (n_layers, batch, hidden_size)
-        # r_out (batch, time_step, hidden_size)
+
         r_out, h_state = self.rnn(x, h_state)
-        outs = [self.fc(r_out[:, step, :]) for step in range(r_out.size(1))]
+        outs = [self.fc(r_out[:, time_step, :]) for time_step in range(r_out.size(1))]
         return torch.stack(outs, dim=1), h_state
 
 
 rnn = RNN()
 print(rnn)
 
-optimizier = torch.optim.Adam(rnn.parameters(), lr=LearningRate)
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
 criterion = nn.MSELoss()
 
+h_state = None
 plt.figure(1, figsize=(12, 5))
 plt.ion()
 
-h_state = None
+
 for step in range(200):
-    start, end = step*np.pi, (step+1)*np.pi
-    steps = np.linspace(start, end, TIME_STEP)
-    x_np = np.sin(step)
-    y_np = np.cos(step)
+    start, end = step * np.pi, (step+1) * np.pi
+    steps = np.linspace(start, end, TIME_STEP, dtype=np.float32, endpoint=False)
+    x_np = np.sin(steps)
+    y_np = np.cos(steps)
 
     x = torch.from_numpy(x_np[np.newaxis, :, np.newaxis])
     y = torch.from_numpy(y_np[np.newaxis, :, np.newaxis])
@@ -51,16 +50,14 @@ for step in range(200):
     h_state = h_state.data
 
     loss = criterion(prediction, y)
-    optimizier.zero_grad()
+    optimizer.zero_grad()
     loss.backward()
-    optimizier.step()
+    optimizer.step()
 
-    plt.plot(steps, y_np.flatten(), 'r-', label='target(cos)')
-    plt.plot(steps, prediction.data.numpy().flatten(), 'b-', label='prediction(sin->cos)')
+    plt.plot(steps, y_np.flatten(), 'r-')
+    plt.plot(steps, prediction.data.numpy().flatten(), 'b-')
     plt.draw()
-    plt.pause(0.05)
-    plt.legend(loc='best')
+    plt.pause(0.02)
 
 plt.ioff()
 plt.show()
-
