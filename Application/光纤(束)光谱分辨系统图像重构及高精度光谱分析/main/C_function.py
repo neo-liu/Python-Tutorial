d92c012e44379b7fb42e606f24cac8a8ef53c1f6
neo-liu <787525970@qq.com>
2020-04-23 20:48:35 +0800

    Add files via upload

--- Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/C_function.py
+++ Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/C_function.py
@@ -1,74 +1,109 @@
 # author: Neo Liu
 # data: 2020.4.9
 # function: 处理光强分布，获取直接的Probe光谱
-from tools import *
+from tools import *  # 导入读取数据、计算需要使用到的函数包
+
+###################################################################################################
+# 定义超参数
 
 # 定义超参数，用来控制整个程序的参数，可以根据不同情况灵活控制
-name1 = './data format.txt'
+name1 = './data format.txt'  # 此为数据格式文件
 name2 = './output_755.0.fld'  # 此为标定谱线的光强分布数据之一，用于读取数据维度使用
-start, end, step = 755.0, 780.0+1e-5, 0.2  # 此为波长参数
-delta_lambda_start, delta_lambda_end, delta_lambda_step = 0.00, 0.05, 0.01
 
-# 波长（横）坐标， 以及平滑C时使用的波长（横）坐标
+# 此为波长覆盖参数（对应与数据表的横向维度），波长范围λ从755.0至780.0nm，波长间隔为0.2nm，共126组
+start, end, step = 755.0, 780.0+1e-4, 0.2  # 用于还原光谱
+
+# 此为波长细微变化参数（对应于数据表的纵向维度），Δλ从0至0.05nm变化，间隔为0.01nm, 共6组
+delta_lambda_start, delta_lambda_end, delta_lambda_step = 0.00, 0.06, 0.01
+
+###################################################################################################
+# 读取数据
+
+# 波长（横）坐标， 以及平滑C时使用的波长（横）坐标，作为光谱相关度曲线的横坐标使用
 delta_lambda = np.arange(0, delta_lambda_end - delta_lambda_start, delta_lambda_step)
 Lambda_Smooth = np.linspace(min(delta_lambda), max(delta_lambda), 1000)
 
-# 读取光强分布的数据格式
+# 读取光强分布的数据格式，用于后边散斑图的整形
 boundary = read_format(name1)
 XY_num_diff = int(boundary[1] - boundary[0])
 
-# 获取光强分布数据的维度
+# 获取光强分布数据的维度，用于后边散斑图的整形和验证读取数据的正确性
 shape = list(get_shape(name2))
 
-# read all data of Intensity distribution
+# 读取所有数据（具体细节我认为不是很重要，而且数据结构上稍稍复杂）
 for num, delta in enumerate(delta_lambda):
     print('Loading data while Δ = ' + str(delta) + '\n # # # # # # # # # # \n')
+    # 每个循环中都读取一个Δλ取值的数据
     for num1, i in enumerate(np.arange(start + delta, end + delta, step)):
         data = read_file('./output_' + str(round(i, 2)) + '.fld', data, shape, multi=True, num=num1)
+    # 对数据做一些整形，展开的处理
     for j in data:
-        j = j[:, int(XY_num_diff/2):int(boundary[1] - XY_num_diff/2)]
-        Data.append(j[1:-1, 1:-1].reshape(1, -1)[0])
-
+        j = j[:, int(XY_num_diff/2):int(boundary[1] - XY_num_diff/2)]  # 将数据整形成方形
+        Data.append(j[:, :].reshape(1, -1)[0])  # 将数据展开成一个一维向量
     Data_set.append(np.expand_dims(Data, axis=0))
     Data.clear()
     data = np.array([])
+
+# Data_set为读取数据的最后结果，数据维度为[6， 126， 75076]
+# 数据说明：为了便于计算，在读取数据后，将二维的散斑数据展开成了一维形式
+# ’6‘维度：由代码第16行控制；’126‘维度：由第13行控制；‘75076’维度：由数据格式控制，代表每个光强分布中包含75076组数据
 Data_set = np.squeeze(Data_set, axis=1)
 
-# calculate correlation
+###################################################################################################
+# 计算光谱相关度
+
+# 光谱相关度计算 C（Δλ，x） = <I(λ，x)I(λ+Δλ，x)> / [<I(λ，x)><I(λ+Δλ，x)>] - 1
+# 其中<>代表对光强在λ上取平均，<I(λ，x)> = [I(λ1，x)+I(λ2，x)+ ... I(λn，x)] / n ；   <I(λ+Δλ，x)>同理
+# <I(λ，x)I(λ+Δλ，x)> = [I(λ1，x)I(λ1+Δλ，x)+I(λ2，x)I(λ2+Δλ，x)+ ... +I(λn，x)I(λn+Δλ，x)] / n
+# 另外对λ取平局后，需要对整体在x上取平均
 for i in Data_set:
-    up = np.mean(np.multiply(Data_set[0], i), axis=0)
-    down = np.multiply(np.mean(Data_set[0], axis=0), np.mean(i, axis=0))
-    Correlation.append(np.mean((up / down), axis=0) - 1)
-for i in np.arange(len(Correlation)):
-    Correlation[i] = Correlation[i] / max(Correlation)
-# smooth operation(cubic)
+    # Data_set[0]为Δλ=0时的光强分布，共包含126组散斑数据，维度形式[126, 75076]
+    # 在循环中i分别表示Δλ=0，0.01，0.02，0.03，0.04，0.05时的光强分布，每个i均包含126组数据，维度形式[126, 75076]
+    # 在求均值令axis=0时，可得到光强分布在λ上的平均值
+    # 注：np.multiply 求解的是两个矩阵元素的乘积，而不是矩阵乘法。矩阵乘法表示为np.dot
+    up = np.mean(np.multiply(Data_set[0], i), axis=0)  # 计算<I(λ，x)I(λ+Δλ，x)>
+    down = np.multiply(np.mean(Data_set[0], axis=0), np.mean(i, axis=0))  # 计算I(λ，x)><I(λ+Δλ，x)>
+    Correlation.append(np.mean((up / down), axis=0) - 1)  # 最后对x维度取均值，计算光谱相关度
+
+# 归一化过程，若不需要则可以注释掉
+# scale = Correlation[0]
+# for i in np.arange(len(Correlation)):
+#     Correlation[i] = Correlation[i] / scale
+
+# 使用插值的方式，将光谱相关度曲线拟合成为二阶、三阶或者更高阶曲线
 Correlation_Smooth = interp1d(delta_lambda, np.array(Correlation), kind='cubic')  # quadratic
 
-# svg display
+###################################################################################################
+# 绘制光谱相关度曲线
+
+# 使用矢量图方式画出拟合曲线
 display.set_matplotlib_formats('svg')
 
-# setup figure and plot
+# 各种图表参数
 plt.figure(figsize=(7, 6))
 plt.plot(Lambda_Smooth, Correlation_Smooth(Lambda_Smooth), linewidth=2, color='green', linestyle='-')
 print(min(Correlation_Smooth(Lambda_Smooth)))
+
 # plot scatter and reference
 for num, i in enumerate(Correlation_Smooth(Lambda_Smooth)):
     if i < (Correlation[0] / 2):
+        # 绘制初值下降一半时的散点
         plt.scatter(Lambda_Smooth[num], (Correlation[0] / 2),
                     color='red', marker='o', linewidths=3)
-        plt.scatter(Lambda_Smooth[2*num], Correlation_Smooth(Lambda_Smooth)[2*num],
-                    color='red', marker='o', linewidths=3)
+        # 绘制光谱相关宽度的位置，如果找不到则会报错
+        # 在运行的时候应该先注释掉，先判断、后使用
+        # plt.scatter(Lambda_Smooth[2*num], Correlation_Smooth(Lambda_Smooth)[2*num],
+        #             color='red', marker='o', linewidths=3)
         # represent resolution
-        resolution = Lambda_Smooth[2*num] - min(Lambda_Smooth)
-        plt.axvline(Lambda_Smooth[2*num], 0, 0.15, linestyle='--')
-        plt.axhline(Correlation_Smooth(Lambda_Smooth)[2*num], 0, 0.06, linestyle='--')
+        # resolution = Lambda_Smooth[2*num] - min(Lambda_Smooth)
+        # plt.axvline(Lambda_Smooth[2*num], 0, 0.15, linestyle='--')
+        # plt.axhline(Correlation_Smooth(Lambda_Smooth)[2*num], 0, 0.06, linestyle='--')
 
         break
 
-# format correlation
+# 在图中标记光谱相关度值
 for num, i in enumerate(Correlation):
     Correlation[num] = float(str(Correlation[num])[:4])
-
 for i in range(len(delta_lambda)):
     plt.annotate(Correlation[i], xy=(delta_lambda[i], Correlation[i]), xytext=(delta_lambda[i] * 1.0001,
                                                                                Correlation[i]*1))
--- Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/tools.py
+++ Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/tools.py
@@ -1,26 +1,34 @@
 # author: Neo Liu
 # data: 2020.4.9
 # function: 处理光强分布，获取直接的Probe光谱
+# 此包中包含的变量、函数是光纤分辨系统中需要的基础函数
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from IPython import display
 from scipy.interpolate import interp1d
 
-plt = plt
-display = display
-interp1d = interp1d
-resolution = 0
-Data = list()
-Correlation = list()
-T_matrix = list()
-boundary = list()
-Intensity_Vector = list()
-Reconstruction_Spectral_Vector = list()
-miu = list()
-Spectral_Vector = list()
+# 变量
+resolution = 0  # 光谱分辨率
+data = np.array([])  # 读取数据的中间变量
+Data = list()  # 读取数据函数中的变量
+Data_set = list()  # 读取的数据集（用于表示所有数据），使用于C_function.py中
+Correlation = list()  # 光谱相关度，使用于C_function.py
+T_matrix = list()  # 传输矩阵，使用于除C_function.py、2D_plot.py、3D_plot.py以外的所有代码中
+boundary = np.array([])  # 单个散斑图样的数据尺度，用与数据整形
+Intensity_Vector = np.array([])  # 特征谱线对应的光强矢量（K: 766.46nm, 769.9nm; Cu: 510.5nm,521.8nm）
+Reconstruction_Spectral_Vector = np.array([])  # 重建光谱矢量，使用传输矩阵和特征谱线光强分布重建的光谱用此变量代替
+miu = np.array([])  # 重建错误率
 
 
+# 函数
+plt = plt  # 绘图库
+display = display  # 绘制矢量图函数
+interp1d = interp1d  # 插值函数
+
+
+# 读取数据维度（指单个散斑图长宽方向的数据个数），用来整形光强分布数据
 def get_shape(name):
     i, j = 0, 0
     with open(str(name), 'r') as file:
@@ -36,6 +44,7 @@ def get_shape(name):
     return raw, col
 
 
+# 于上述函数类似，应该合并在一起
 def read_format(name):
     data = list()
     with open(str(name), 'r') as f:
@@ -46,7 +55,8 @@ def read_format(name):
     return np.array((data[0][0], data[1][0]))
 
 
-def read_file(name, shape=None, multi=True, num=None):
+# 读取数据，可视情况读取单个或多组数据
+def read_file(name, Data=None, shape=None, multi=True, num=None):
 
     with open(str(name), 'r') as f:
         data = np.zeros(shape=shape)
@@ -60,10 +70,11 @@ def read_file(name, shape=None, multi=True, num=None):
         if num and num % 20 == 0:
             print('读取' + str(num) + '组数据 完成 . . . \n # # # # # # # # # # \n')
     if not multi:
-
         return data
     elif multi:
-        Data.append(data)
+        data = np.expand_dims(data, axis=0)
+        if len(Data)==0:
+            Data = data
+        else:
+            Data = np.append(Data, data, axis=0)
         return Data
-
-
