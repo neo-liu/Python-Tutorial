a68cf50eab3e49cf48688e7cd3135517d26934a4
neo-liu <787525970@qq.com>
2020-04-25 21:01:21 +0800

    Enable

--- Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/transmission_matrix(directly).py
+++ Application/光纤(束)光谱分辨系统图像重构及高精度光谱分析/main/transmission_matrix(directly).py
@@ -3,13 +3,16 @@
 # function: 处理光强分布，获取直接的Probe光谱
 from tools import *
 
+###################################################################################################
+# 定义超参数
+
 # 定义超参数，用来控制整个程序的参数，可以根据不同情况灵活控制
 name1 = './data format.txt'  # 此为光强分布的数据格式
-name2 = './output_766.46.fld'  # 此为两个Probe谱线的光强分布数据
-name3 = './output_769.9.fld'
-start, end, step = 755.0, 780.0, 0.2  # 此为波长参数
-sample_step = 30  # 此为光强分布的抽样间隔
-threshold = 0.01  # 此为重建精确光谱时使用的阈值，见61 line
+name2 = './output_766.46.fld'  # 此为两个Probe谱线的光强分布数据。若为铜原子则改为'./output_510.5.fld'
+name3 = './output_769.9.fld'  # 若为铜原子则改为'./output_521.8.fld'
+start, end, step = 755.0, 780.0, 0.2  # 可认为光纤分辨系统带宽为755-780nm。若为铜原子则改为508.0， 523.0，0.2
+sample_step = 30  # 此为光强分布的抽样间隔,完整的光强分布在做奇异值变换的时候对计算机性能要求很严格，需要一定程度的抽样
+threshold = 0.01  # 此为重建精确光谱时使用的阈值，光谱向量中太小的数值不具有参考价值
 
 # 读取光强分布的数据格式
 boundary = read_format(name1)
@@ -22,13 +25,14 @@ if shape[0] / boundary[0] == 1.0 and shape[1] / boundary[1] == 1.0:
 else:
     raise Exception('数据维度不合理\n # # # # # # # # # #\n')
 
-# 读取name1, name2中的光强数据并抽样， 如果只使用一个数据则可以注释掉包含 *2 的部分
+# 读取name1, name2中的光强数据并抽样， 如果只使用一个数据则可以注释掉包含Intensity_Vector2的部分
+# Intensity_Vector1、Intensity_Vector2分别代表两个原子特征谱线下的散斑光强分布
 Intensity_Vector1 = read_file(name2, Data=None, shape=shape, multi=False)
 # Intensity_Vector2 = read_file(name3, shape, multi=False)
 # Intensity_Vector = (Intensity_Vector1 + Intensity_Vector2)[:, int(
 #     XY_num_diff/2):int(boundary[1][0] - XY_num_diff/2)].reshape(-1, 1)[:: sample_step]
 Intensity_Vector = Intensity_Vector1[:, int(XY_num_diff/2):int(
-    boundary[1] - XY_num_diff/2)].reshape(-1, 1)[:: sample_step]
+    boundary[1] - XY_num_diff/2)].reshape(-1, 1)[:: sample_step]  # 依次进行数据整形、展开成一维、抽样
 
 # 读取标定光强数据并抽样
 for num, i in enumerate(np.arange(start, end, step)):
@@ -36,6 +40,8 @@ for num, i in enumerate(np.arange(start, end, step)):
 for num, i in enumerate(data):
     i = i[:, int(XY_num_diff/2):int(boundary[1] - XY_num_diff/2)].reshape(1, -1)
     T_matrix.append(i[0, ::sample_step])
+
+###################################################################################################
 
 # 计算传输矩阵的伪逆矩阵（包含了奇异值分解）
 T_matrix = np.array(T_matrix).T
@@ -67,7 +73,7 @@ for num in range(len(Spectral_Vector1)):
         a, b = 0, 0
 
 # 绘制精确还原光谱
-plt.figure(0)
+plt.figure(' 精确还原光谱')
 plt.stem(Spectral_plot2, Spectral_Vector2, use_line_collection=True, linefmt='r--', basefmt='b--')
 for num, i in enumerate(Spectral_Vector2):
     i = round(i.tolist()[0], 2)
@@ -77,7 +83,7 @@ for num, i in enumerate(Spectral_Vector2):
                      xytext=(Spectral_plot2[num], i))
 
 # 绘制完整还原光谱
-plt.figure(1)
+plt.figure('原始还原光谱')
 plt.stem(Spectral_plot1, Spectral_Vector1, use_line_collection=True, linefmt='r--', basefmt='b--')
 for num, i in enumerate(Spectral_Vector1):
     i = round(i.tolist()[0], 2)
